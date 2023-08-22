f02a7d0cd071f53a994297a9dd82e61d1b846dbf
yawempire <manuelasantempire@gmail.com>
2023-08-22 19:00:10 +0000

    output

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+  * _printf - print printf function
+  * @format: Arg
+  *
+  * Return: str to output
+  */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0' || *format == ' ')
+				break;
+
+			if (*format == 'c')
+				count += handle_c(args);
+			else if (*format == 's')
+				count += handle_s(args);
+			else if (*format == '%')
+				count += print_char('%');
+		}
+		else
+		{
+			count += print_char(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
